b9a3e2881b6cf727a0b5883466dc5fd018acae9b
Dareenelalfy <dareenelalfy3@gmail.com>
2024-05-10 17:09:08 +0300

    First commit

---
+++ src/components/brand/imports.js
@@ -0,0 +1,15 @@
+import React from "react";
+import slack from "../../assets/slack.png"
+import atlassian from "../../assets/atlassian.png"
+import dropbox from "../../assets/dropbox.png"
+import google from "../../assets/google.png"
+import shopify from "../../assets/shopify.png"
+
+export {
+    slack,
+    atlassian,
+    dropbox,
+    shopify,
+    google
+
+}
